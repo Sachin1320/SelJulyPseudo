a83b61de796a38c0f3292990d76a316910d892d0
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-12-12 18:25:57 +0530

    Update SortingUsingCollection.java

--- week3/day2/classroom/SortingUsingCollection.java
+++ week3/day2/classroom/SortingUsingCollection.java
@@ -3,7 +3,20 @@ package week3.day2.classroom;
 public class SortingUsingCollection {
 
 	public static void main(String[] args) {
+        
+		
+		List<String> input=new ArrayList<String>();
+		
+		input.add("TCS");
+		input.add("wipro");
+		input.add("cts");
+		input.add("aspire");
+		Collections.sort(input);
+		Collections.reverse(input);
+		System.out.println(input);
+	}
 
+}
 		String[] input = {"HCL","Wipro","Aspire Systems","CTS"};
 //		Declare a String array and add the Strings values as (HCL, Wipro,  Aspire Systems, CTS)		
 
